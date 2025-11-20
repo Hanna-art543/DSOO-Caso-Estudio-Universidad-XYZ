8a98d6cd4699cc5d7cf275c116d50419f95b075c
Hanna-art543 <amirandalo@unsa.edu.pe>
2025-11-20 11:36:23 -0500

    Modificaciones en la clase

--- Curso.java
+++ Curso.java
@@ -2,11 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Curso {
+    // Atributos
     private String nombre;
     private String codigo;
     private int creditos;
     private int semestre;
     private Carrera carrera; 
+    // Listas para manejar relaciones "uno a muchos"
     private List<PreRequisito> preRequisitos;
     private List<Nota> notas;
 
@@ -16,12 +18,14 @@ public class Curso {
         this.creditos = creditos;
         this.semestre = semestre;
         this.carrera = carrera;
+        //Inicializamos las listas vacías 
         this.preRequisitos = new ArrayList<>();
         this.notas = new ArrayList<>();
     }
 
-    //Métodos para agregar, eliminar cursos
+    // Métodos 
     public boolean agregarPreRequisito(PreRequisito p) {
+        // Evitamos duplicados antes de agregar
         if (!preRequisitos.contains(p)) {
             preRequisitos.add(p);
             return true;
@@ -30,20 +34,24 @@ public class Curso {
     }
 
     public boolean eliminarPreRequisito(String id) {
+        // Uso de Lambda, elimina directo si el ID coincide (ignorando mayúsculas)
         return this.preRequisitos.removeIf(p -> p.getID().equalsIgnoreCase(id));
     }
 
     public boolean cumpleRequisitos(Alumno a) {
         for (PreRequisito p : preRequisitos) {
+            // Si falla en UNO solo, retorna falso inmediatamente
             if (!p.verificarCumplimiento(a)) {
                 return false;
             }
         }
-        return true;
+        return true; // Si terminó el bucle, es que cumplió todo
     }
 
     public double calcularPromedioNotas() {
+        // Validación de seguridad
         if (notas.isEmpty()) return 0.0;
+        
         double suma = 0;
         for (Nota n : notas) {
             suma += n.getValor();
@@ -61,6 +69,7 @@ public class Curso {
         System.out.println("Código: " + codigo);
         System.out.println("Créditos: " + creditos);
         System.out.println("Semestre: " + semestre);
+        // Operador ternario: Evita error si la carrera aún no fue asignada (null)
         System.out.println("Carrera: " + (carrera != null ? carrera.getNombre() : "No asignada"));
     }
 
